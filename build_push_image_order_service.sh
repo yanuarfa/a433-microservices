e0b6303331532205545f19dc23123d6461aacb2a
Yanuar Fa <yanuarfatur41@gmail.com>
2023-11-25 23:38:47 +0700

    Added CI to build image and upload it to ghcr

---
+++ build_push_image_order_service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Membuild image sesuai dengan konfigurasi dan menggunakan tag sesuai github container registry
+docker build -t ghcr.io/yanuarfa/order-service:latest .
+
+# Login ke github container registry
+echo $GHCR_TOKEN | docker login ghcr.io --username yanuarfa --password-stdin
+
+# Push image ke github container registry
+docker push ghcr.io/yanuarfa/order-service:latest
